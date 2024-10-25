7d0ebda6233b3087b41d7e2b475130fb5794e046
Ajay <ajay.singh.114085@gmail.com>
2024-10-26 01:07:58 +0530

    Service commited

---
+++ src/main/java/com/kafka/config/KafkaTopicConfig.java
@@ -0,0 +1,28 @@
+package com.kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+
+    @Value("${spring.kafka.json.topic.name}")
+    private String jsonTopicName;
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name(topicName)
+                .build();
+    }
+
+    @Bean
+    public NewTopic jsonTopic(){
+        return TopicBuilder.name(jsonTopicName)
+                .build();
+    }
+}
